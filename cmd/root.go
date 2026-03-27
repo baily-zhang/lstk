5bcff25dd2ddc2dda9bc2e3f51ded909e58f268a
George Tsiolis <tsiolis.g@gmail.com>
2026-03-27 15:56:35 +0200

    Replace profile option with loading spinner

--- cmd/root.go
+++ cmd/root.go
@@ -62,6 +62,7 @@ func NewRootCmd(cfg *env.Env, tel *telemetry.Client, logger log.Logger) *cobra.C
 		newConfigCmd(cfg, tel),
 		newUpdateCmd(cfg, tel),
 		newDocsCmd(),
+		newAWSCmd(cfg, tel),
 	)
 
 	return root
